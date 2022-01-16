a15a33ebc8f93becfc9f49586f6cb6a9adf5cf10
IanOleg <ianoleg86@gmail.com>
2022-01-16 19:42:34 +0300

    hello everybody

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -1,6 +1,6 @@
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("Hello everybody!");
     }
 
     public void testMetod(){
